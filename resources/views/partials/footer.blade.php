8611e27286218c6fba618cd24b8e2537248addce
Ryan <ryan@localhost.localdomain>
2018-05-25 12:21:48 +0200

    Add footer layout and basic style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="content-info">
+<footer class="footer">
   <div class="container">
     <div class="row">
       <ul class="social-icons">
@@ -7,31 +7,13 @@
         <li><img src="https://www.blackwatercottagebandb.co.uk/wp-content/themes/slate/images/social-icon-tripadvisor.png"></li>
       </ul>
     </div>
-  	<div class="row" id="footer-content">
+  	<div class="row footer-content">
       <div class="col-md-6" id="footer-copyright">
         &copy; Copyright Blackwater Cottage 2013
       </div>
-      <div class="col-md-6" id="footer-menu">
+      <div class="col-md-6 footer-menu">
         <a href="#">Terms & Conditions</a>
       </div>
 	  </div>
   </div>
 </footer>
-<style>
-.content-info {
-	margin-top: 24px;
-	border-top: 10px solid whitesmoke;
-	background: url(https://www.blackwatercottagebandb.co.uk/wp-content/themes/slate/images/bg-slate.png) top left repeat-x #C5D9DC;
-	height: 350px;
-  padding: 100px 0;
-}
-
-#footer-copyright {
-  text-align: left;
-}
-
-#footer-menu {
-  text-align: right;
-}
-
-</style>
